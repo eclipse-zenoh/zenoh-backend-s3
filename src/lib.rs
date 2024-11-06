b59ff63e3f683158bb588079d337acdefa8f4cd9
Michael Ilyin <milyin@gmail.com>
2024-11-06 14:19:45 +0100

    remove obsolete `Value` (#219)
    * remove obsolete `Value`
    
    * cargo fmt
    
    * linked to main branch

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,8 @@ use config::{S3Config, TlsClientConfig, TLS_PROP};
 use futures::{future::join_all, stream::FuturesUnordered};
 use utils::S3Key;
 use zenoh::{
-    bytes::Encoding,
-    internal::{zerror, Value},
+    bytes::{Encoding, ZBytes},
+    internal::zerror,
     key_expr::OwnedKeyExpr,
     query::Parameters,
     time::Timestamp,
@@ -237,8 +237,12 @@ impl Storage for S3Storage {
         let s3_key = S3Key::from_key_expr(self.config.path_prefix.as_ref(), key.to_owned())?;
 
         let get_result = self.get_stored_value(&s3_key.into()).await?;
-        if let Some((timestamp, value)) = get_result {
-            let stored_data = StoredData { value, timestamp };
+        if let Some((payload, encoding, timestamp)) = get_result {
+            let stored_data = StoredData {
+                payload,
+                encoding,
+                timestamp,
+            };
             Ok(vec![stored_data])
         } else {
             Ok(vec![])
@@ -249,7 +253,8 @@ impl Storage for S3Storage {
     async fn put(
         &mut self,
         key: Option<OwnedKeyExpr>,
-        value: Value,
+        payload: ZBytes,
+        encoding: Encoding,
         timestamp: Timestamp,
     ) -> ZResult<StorageInsertionResult> {
         let key = key.map_or_else(|| OwnedKeyExpr::from_str(NONE_KEY), Ok)?;
@@ -263,8 +268,12 @@ impl Storage for S3Storage {
 
             let key: String = s3_key.into();
             let client = self.client.clone();
-            await_task!(client.put_object(key, value, Some(metadata)).await,)
-                .map_err(|e| zerror!("Put operation failed: {e}"))?;
+            await_task!(
+                client
+                    .put_object(key, payload, encoding, Some(metadata))
+                    .await,
+            )
+            .map_err(|e| zerror!("Put operation failed: {e}"))?;
 
             Ok(StorageInsertionResult::Inserted)
         } else {
@@ -381,7 +390,10 @@ impl Storage for S3Storage {
 }
 
 impl S3Storage {
-    async fn get_stored_value(&self, key: &String) -> ZResult<Option<(Timestamp, Value)>> {
+    async fn get_stored_value(
+        &self,
+        key: &String,
+    ) -> ZResult<Option<(ZBytes, Encoding, Timestamp)>> {
         let client = self.client.clone();
         let res = await_task!(client.get_object(key.as_str()).await, key);
 
@@ -418,7 +430,7 @@ impl S3Storage {
                 zerror!("Get operation failed. Couldn't process retrieved contents: {e}")
             }),)?;
 
-        Ok(Some((timestamp, Value::new(Vec::from(bytes), encoding))))
+        Ok(Some((bytes.into(), encoding, timestamp)))
     }
 }
 
